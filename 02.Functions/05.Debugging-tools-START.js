121c34ec2133175769c82e55b91e2a59015ae15c
Slobodan Vlaic <slobiiv@gmail.com>
2019-12-10 00:06:58 +0000

    debugger

--- 02.Functions/05.Debugging-tools-START.js
+++ 02.Functions/05.Debugging-tools-START.js
@@ -8,7 +8,7 @@ people.forEach((person, index) => {
   console.log(person.name);
 });
 
-// Console methods (BASIC - Wes is using these)
+// 1.Console methods (BASIC - Wes is using these)
 /*
     console.log();
     console.warn();
@@ -16,4 +16,41 @@ people.forEach((person, index) => {
   * console.groupCollapsed(); .... ; console.groupEnd();helps when there is lot's of stuff to console.
     console.count(); counts how many times the function runs.
     console.table(); makes a table format out of data in console.
+*/
+function doctorize(name) {
+  return `Dr. ${name}`;
+}
+
+function greet(name) {
+  // doesntExist();
+  return `Hello ${name}`;
+}
+
+function go() {
+  debugger;
+  const name = doctorize(greet('Slobodan'));
+  console.log(name);
+}
+
+// 2. Callstack, Stack Trace - The stack trace will tell you what function called what function
+/*
+    THIS IS WHERE THE GOOD STUFF IS. IF YOU WANT TO KNOW WHAT WENT WRONG, YOU HAVE TO GET GOOD AT READING THESE CALL STACKS
+    ReferenceError: doesntExist is not defined
+    debugger eval code:25:3
+    greet http://127.0.0.1:5500/02.Functions/05.Debugging-tools-START.js:25
+    go http://127.0.0.1:5500/02.Functions/05.Debugging-tools-START.js:30
+    <anonymous> debugger eval code:1
+ */
+
+// 3. Grabbing Elements (Handy one)
+/*
+    If you go on the website that you want to figure out how it works follow these steps:
+      a) Enter the Developer mode CTRL+SHIFT+C
+      b) Click on the element you want to debugg
+      c) Select Console window(or CTRL+SHIFT+i), and type $0. It will copy the whole element data that's been selected previously. 0(zero) means the last element clicked...
+
+    Inside console :
+     $('p') will give us document.queryselector and that will match the first thing that matches the selector here('p').
+     $$('p') will match all of the ones that match selector. It founds all of the paragraph tags, in this case.
+
 */
