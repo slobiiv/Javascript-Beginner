bfefcd205e6c40b90a00ed6e83a22dbe14a3cdb5
Slobodan Vlaic <slobiiv@gmail.com>
2020-01-14 22:27:26 +0000

    terms of condition scroll

---
+++ 06.Serious-Practice-Exercises/03.Scroll-Events-and-Intersection-Observer.js
@@ -0,0 +1,10 @@
+/* A scroll event is when somebody goes ahead and scrolls on the page, or scrolls inside of an element. */
+/*
+      A pretty common thing that I've had to code a couple times in my life, and I'm sure you'll have to code,
+      is TERMS AND CONDITIONS, scroll to accept.
+*/
+console.log('it might works');
+/*
+      If you wanna listen for a window scroll event, you just listened for window dot addEventListener.
+      But if it's a case of
+*/
